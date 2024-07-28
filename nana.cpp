1e8624aaa1663430e5af80d41e4ac608c0983ed5
Felix Xing <felixaszx@outlook.com>
2024-07-28 16:39:31 +0400

    nana startup done

--- nana.cpp
+++ nana.cpp
@@ -2,20 +2,23 @@
 #include <future>
 #include <chrono>
 #include <iostream>
+#include <windows.h>
 
 #include <nana/gui.hpp>
 #include <nana/gui/widgets/button.hpp>
+#include <nana/gui/widgets/menubar.hpp>
 
 int main(int argc, char** argv)
 {
+    SetProcessDPIAware();
     std::atomic_bool nana_running = true;
     std::thread nana_th(
         [&]()
         {
             nana::form fm({}, {800, 600});
-            nana::button btn(fm, nana::rectangle{0, 0, 200, 200});
-            btn.caption("hello nana");
-            btn.events().click.connect([]() { std::cout << 1; });
+            nana::menubar menubar(fm);
+            menubar.push_back("File").append("New", [](nana::menu::item_proxy& item) { std::cout << item.text(); });
+
             fm.show();
             nana::exec();
             nana_running = false;
