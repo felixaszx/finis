95d1e7bbb402e12ff372f9db5c01efb2112e160d
Felix Xing <felixaszx@outlook.com>
2024-07-30 18:36:16 -0400

    doing audio layer now

--- app/main.cpp
+++ app/main.cpp
@@ -25,24 +25,6 @@ int main(int argc, char** argv)
     Swapchain sc;
     sc.create();
 
-    TextureMgr texture_mgr;
-    PipelineMgr pipeline_mgr;
-    RenderMgr render_mgr;
-    AnimationMgr animation_mgr;
-    gltf::Expected<gltf::GltfDataBuffer> gltf_file({});
-    auto sponza = render_mgr.upload_res("res/models/sponza_gltf/sponza.glb", texture_mgr, animation_mgr, gltf_file);
-    render_mgr.lock_and_prepared();
-
-    GBufferPass g_buffer;
-    g_buffer(pipeline_mgr, render_mgr);
-
-    vk::RenderingInfo rendering{};
-    rendering.setColorAttachments(g_buffer.color_infos_);
-    rendering.pDepthAttachment = &g_buffer.depth_stencil_info_;
-    rendering.pStencilAttachment = &g_buffer.depth_stencil_info_;
-    rendering.layerCount = 1;
-    rendering.renderArea = vk::Rect2D{{}, {1920, 1080}};
-
     Semaphore next_img;
     Semaphore submit;
     Fence frame_fence;
@@ -58,13 +40,6 @@ int main(int argc, char** argv)
     cmd_alloc.level = vk::CommandBufferLevel::ePrimary;
     auto cmds = g.device().allocateCommandBuffers(cmd_alloc);
 
-    struct
-    {
-        glm::mat4 model = glm::scale(glm::vec3(0.1, 0.1, 0.1));
-        glm::mat4 view = glm::lookAt(glm::vec3(0, 0, 0), glm::vec3(1, 0, 0), glm::vec3(0, 0, 1));
-        glm::mat4 proj = glm::perspective(glm::radians(45.0f), float(1920) / 1080, 0.1f, 1000.0f);
-    } push;
-
     while (g.update())
     {
         fle::Global::check();
@@ -79,11 +54,6 @@ int main(int argc, char** argv)
             g.update();
         }
 
-        if (glfwGetKey(g.window(), GLFW_KEY_L))
-        {
-            g_buffer.hot_reload(pipeline_mgr);
-        }
-
         if (glfwGetKey(g.window(), GLFW_KEY_P))
         {
             fltk.show();
@@ -91,27 +61,6 @@ int main(int argc, char** argv)
 
         cmds[0].reset();
         begin_cmd(cmds[0]);
-        cmds[0].beginRendering(rendering);
-        render_mgr.draw({sponza.first},
-                        [&](vk::Buffer device_buffer, //
-                            uint32_t vtx_buffer_binding,
-                            const RenderMgr::VtxIdxBufferExtra& offsets, //
-                            vk::Buffer host_buffer,
-                            const RenderMgr::HostBufferExtra& host_offsets, //
-                            vk::DescriptorSet texture_set)
-                        {
-                            pipeline_mgr.bind_pipeline(cmds[0], g_buffer.pipeline_);
-                            pipeline_mgr.bind_descriptor_sets(cmds[0], texture_set);
-                            cmds[0].pushConstants(g_buffer.p_layout_, vk::ShaderStageFlagBits::eVertex, 0, sizeof(push),
-                                                  &push);
-                            cmds[0].setViewport(0, vk::Viewport(0, 0, 1920, 1080, 0, 1));
-                            cmds[0].setScissor(0, vk::Rect2D({}, {1920, 1080}));
-                            cmds[0].bindVertexBuffers(vtx_buffer_binding, device_buffer, {offsets.vtx_offset_});
-                            cmds[0].bindIndexBuffer(device_buffer, offsets.idx_offset_, vk::IndexType::eUint32);
-                            cmds[0].drawIndexedIndirect(host_buffer, host_offsets.draw_call_offset_, sponza.second,
-                                                        sizeof(vk::DrawIndexedIndirectCommand));
-                        });
-        cmds[0].endRendering();
         cmds[0].end();
 
         std::vector<vk::PipelineStageFlags> waiting_stages = {vk::PipelineStageFlagBits::eColorAttachmentOutput};
