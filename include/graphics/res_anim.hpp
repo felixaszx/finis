e60de847de3449b410c12143aaebdba1bb8b2326
Felix Xing <felixaszx@outlook.com>
2024-08-14 15:11:14 -0400

    working on animation now

---
+++ include/graphics/res_anim.hpp
@@ -0,0 +1,16 @@
+#ifndef GRAPHICS_RES_ANIM_HPP
+#define GRAPHICS_RES_ANIM_HPP
+
+#include "res_loader.hpp"
+#include "res_structure.hpp"
+
+namespace fi
+{
+    class ResAnimations
+    {
+      private:
+      public:
+    };
+}; // namespace fi
+
+#endif // GRAPHICS_RES_ANIM_HPP
---
+++ src/graphics/res_anim.cpp
@@ -0,0 +1,1 @@
+#include "graphics/res_anim.hpp"
