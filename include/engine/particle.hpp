2b6c20981cfb703670ebace263f1fa7bf4897fa8
Felix Xing <felixaszx@outlook.com>
2024-08-18 22:00:46 -0400

    paeticl;e system done

--- include/engine/particle.hpp
+++ include/engine/particle.hpp
@@ -7,7 +7,7 @@
 
 namespace fi
 {
-    class ParticleGroup
+    class ParticleGroup : private GraphicsObject
     {
       public:
         struct alignas(16) Particle
@@ -30,26 +30,36 @@ namespace fi
                 AWAY    // Moving away from the adjustment
             };
 
-            glm::vec4 adjustment_ = {0, 0, 0, 1}; // [3] use as mode
-            glm::vec4 color_ = {1, 1, 1, 1};      // color apply to particle
+            glm::vec4 adjustment_ = {0, 0, 0, Mode::TOWARD}; // [3] use as mode
+            glm::vec4 color_ = {1, 1, 1, 1};                 // color apply to particle
 
             void set_target_mode(Mode mode) { adjustment_[3] = mode; }
         };
 
       private:
-        std::vector<Particle> particles_{}; // binding 0
-        std::vector<Target> targets_{};     // binding 1
-
-      public:
         struct BufferOffsets
         {
             vk::DeviceSize targets_ = 0;
             vk::DeviceSize uniforms_ = 0;
         };
 
+        bool locked_ = false;
+
+        std::vector<Particle> particles_{}; // binding 0
+        std::vector<Target> targets_{};     // binding 1
+
+      public:
         // buffers
         std::unique_ptr<Buffer<BufferBase::EmptyExtraInfo, storage>> device_buffer_{};
         std::unique_ptr<Buffer<BufferOffsets, storage, uniform, host_coherent, seq_write>> host_buffer_{};
+
+        struct
+        {
+            uint32_t particle_count_ = EMPTY;
+            uint32_t target_count_ = EMPTY;
+            uint32_t padding0_ = EMPTY;
+            uint32_t padding1_ = EMPTY;
+        } uniform_data;
 
         // descriptors
         std::array<vk::DescriptorPoolSize, 2> des_sizes_{};
@@ -57,12 +67,16 @@ namespace fi
         vk::DescriptorSet des_set_{};
 
         ParticleGroup(uint32_t max_particles, uint32_t max_targets = 1);
-        ParticleGroup(vk::Buffer positions, vk::DeviceSize offset, vk::DeviceSize range); // in vec3 only
         ~ParticleGroup();
 
+        void lock_and_load();
+        void update_buffer();
+        [[nodiscard]] bool locked() const { return locked_; }
+        [[nodiscard]] Particle& index_particle(uint32_t idx) { return particles_[idx]; }
+        [[nodiscard]] Target& index_target(uint32_t idx) { return targets_[idx]; }
         void bind_res(vk::CommandBuffer cmd, vk::PipelineBindPoint bind_point, //
                       vk::PipelineLayout pipeline_layout, uint32_t des_set);
-        void compute(vk::CommandBuffer cmd);
+        void compute(vk::CommandBuffer cmd, const glm::vec3& work_group);
     };
 }; // namespace fi
 
