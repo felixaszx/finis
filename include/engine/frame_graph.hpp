6f44d5345be74191d08f4724699bd1f9c5d5c8a8
Felix Xing <felixaszx@outlook.com>
2024-08-20 17:50:09 -0400

    half done

--- include/engine/frame_graph.hpp
+++ include/engine/frame_graph.hpp
@@ -1,6 +1,7 @@
 #ifndef ENGINE_FRAME_GRAPH_HPP
 #define ENGINE_FRAME_GRAPH_HPP
 
+#include <map>
 #include <set>
 
 #include "tools.hpp"
@@ -14,19 +15,74 @@
 
 namespace fi
 {
-    struct FrameImage
+    using PassIdx = uint32_t;
+    using ImgRefIdx = uint32_t;
+    using PassFunc = std::function<void(vk::CommandBuffer cmd)>;
+
+    struct FrameImage : public vk::Image, //
+                        public vma::Allocation
     {
+        vk::Extent3D extent_{};
+        vk::Format format_{};
+        vk::ImageType type_{};
+        vk::ImageUsageFlags usage_{};
+        vk::ImageLayout init_layout_{};
+        vk::ImageSubresource sub_resources_{};
+    };
+
+    struct FrameImageRef : public vk::ImageView
+    {
+        vk::Image image_ = nullptr;
+        vk::ImageViewType type_{};
+        vk::ImageSubresourceRange range_{};
+        std::map<ImgRefIdx, std::pair<vk::ImageLayout, vk::AccessFlags2>> history_;
+
+       static bool is_writing(vk::ImageLayout layout);
     };
 
     struct FramePass
     {
+        vk::Event event_{};
+        vk::DependencyInfo deps_{};
+        std::vector<vk::ImageMemoryBarrier2> img_barriers_{};
+        PassFunc func_ = {};
     };
 
     // this frame graph only support dynamic rendering
     struct FrameGraph : private GraphicsObject
     {
-      private:
       public:
+        using DepthStencilOpMask = uint32_t;
+        enum DepthStencilOps : uint32_t
+        {
+            DEPTH_WRITE = bit_shift_left(0),
+            DEPTH_READ = bit_shift_left(1),
+            STENCIL_WRITE = bit_shift_left(2),
+            STENCIL_READ = bit_shift_left(3)
+        };
+
+      private:
+        bool loaded = false;
+        std::vector<FrameImage> imgs_{};
+        std::vector<FrameImageRef> img_refs_{};
+        std::vector<FramePass> passes_{};
+
+      public:
+        FrameImage& register_image();
+        ImgRefIdx register_image_ref(FrameImageRef& ref_detail);
+        PassIdx register_pass();
+
+        void set_pass_func(PassIdx pass_idx, const PassFunc& func);
+        void read_image(PassIdx pass_idx, ImgRefIdx ref_idx);
+        void write_image(PassIdx pass_idx, ImgRefIdx ref_idx);
+        void pass_image(PassIdx pass_idx, ImgRefIdx ref_idx, vk::ImageLayout layout);
+        void sample_image(PassIdx pass_idx, ImgRefIdx ref_idx);
+        void write_color_atchm(PassIdx pass_idx, ImgRefIdx ref_idx);
+        void set_depth_stencil(PassIdx pass_idx, ImgRefIdx ref_idx, DepthStencilOpMask operations);
+
+        void reset();
+        void compile();
+        void excute();
     };
 }; // namespace fi
 
--- src/engine/frame_graph.cpp
+++ src/engine/frame_graph.cpp
@@ -1,1 +1,144 @@
 #include "engine/frame_graph.hpp"
+
+bool fi::FrameImageRef::is_writing(vk::ImageLayout layout)
+{
+    switch (layout)
+    {
+    case vk::ImageLayout::eShaderReadOnlyOptimal:
+    case vk::ImageLayout::eDepthStencilReadOnlyOptimal:
+    case vk::ImageLayout::eTransferDstOptimal:
+    case vk::ImageLayout::eDepthReadOnlyOptimal:
+    case vk::ImageLayout::eStencilReadOnlyOptimal:
+    case vk::ImageLayout::eReadOnlyOptimal:
+    case vk::ImageLayout::ePresentSrcKHR:
+        return false;
+    case vk::ImageLayout::eGeneral:
+    case vk::ImageLayout::eTransferSrcOptimal:
+    case vk::ImageLayout::eDepthReadOnlyStencilAttachmentOptimal:
+    case vk::ImageLayout::eDepthAttachmentStencilReadOnlyOptimal:
+    case vk::ImageLayout::eDepthAttachmentOptimal:
+    case vk::ImageLayout::eAttachmentOptimal:
+    case vk::ImageLayout::eStencilAttachmentOptimal:
+    case vk::ImageLayout::eDepthStencilAttachmentOptimal:
+    case vk::ImageLayout::eColorAttachmentOptimal:
+    default:
+        return true;
+    }
+}
+
+fi::FrameImage& fi::FrameGraph::register_image()
+{
+    return imgs_.emplace_back();
+}
+
+fi::ImgRefIdx fi::FrameGraph::register_image_ref(FrameImageRef& ref_detail)
+{
+    ref_detail = img_refs_.emplace_back();
+    return img_refs_.size() - 1;
+}
+
+fi::PassIdx fi::FrameGraph::register_pass()
+{
+    passes_.emplace_back();
+    return passes_.size() - 1;
+}
+
+void fi::FrameGraph::set_pass_func(PassIdx pass_idx, const PassFunc& func)
+{
+    passes_[pass_idx].func_ = func;
+}
+
+void fi::FrameGraph::read_image(PassIdx pass_idx, ImgRefIdx ref_idx)
+{
+    img_refs_[ref_idx].history_.emplace(pass_idx,                                          //
+                                        std::pair(vk::ImageLayout::eShaderReadOnlyOptimal, //
+                                                  vk::AccessFlagBits2::eShaderStorageRead));
+}
+
+void fi::FrameGraph::write_image(PassIdx pass_idx, ImgRefIdx ref_idx)
+{
+    img_refs_[ref_idx].history_.emplace(pass_idx,                            //
+                                        std::pair(vk::ImageLayout::eGeneral, //
+                                                  vk::AccessFlagBits2::eShaderStorageWrite));
+}
+
+void fi::FrameGraph::pass_image(PassIdx pass_idx, ImgRefIdx ref_idx, vk::ImageLayout layout)
+{
+    img_refs_[ref_idx].history_.emplace(pass_idx, std::pair(layout, vk::AccessFlagBits2::eMemoryRead));
+}
+
+void fi::FrameGraph::sample_image(PassIdx pass_idx, ImgRefIdx ref_idx)
+{
+    img_refs_[ref_idx].history_.emplace(pass_idx,                                          //
+                                        std::pair(vk::ImageLayout::eShaderReadOnlyOptimal, //
+                                                  vk::AccessFlagBits2::eShaderSampledRead));
+}
+
+void fi::FrameGraph::write_color_atchm(PassIdx pass_idx, ImgRefIdx ref_idx)
+{
+    img_refs_[ref_idx].history_.emplace(pass_idx,                                           //
+                                        std::pair(vk::ImageLayout::eColorAttachmentOptimal, //
+                                                  vk::AccessFlagBits2::eColorAttachmentWrite));
+}
+
+void fi::FrameGraph::set_depth_stencil(PassIdx pass_idx, ImgRefIdx ref_idx, DepthStencilOpMask operations)
+{
+    switch (operations)
+    {
+    case DEPTH_READ:
+        img_refs_[ref_idx].history_.emplace(pass_idx,                                         //
+                                            std::pair(vk::ImageLayout::eDepthReadOnlyOptimal, //
+                                                      vk::AccessFlagBits2::eDepthStencilAttachmentRead));
+        break;
+    case DEPTH_WRITE:
+        img_refs_[ref_idx].history_.emplace(pass_idx,                                           //
+                                            std::pair(vk::ImageLayout::eDepthAttachmentOptimal, //
+                                                      vk::AccessFlagBits2::eDepthStencilAttachmentWrite));
+        break;
+    case STENCIL_READ:
+        img_refs_[ref_idx].history_.emplace(pass_idx,                                           //
+                                            std::pair(vk::ImageLayout::eStencilReadOnlyOptimal, //
+                                                      vk::AccessFlagBits2::eDepthStencilAttachmentRead));
+        break;
+    case STENCIL_WRITE:
+        img_refs_[ref_idx].history_.emplace(pass_idx,                                             //
+                                            std::pair(vk::ImageLayout::eStencilAttachmentOptimal, //
+                                                      vk::AccessFlagBits2::eDepthStencilAttachmentWrite));
+        break;
+    case DEPTH_READ | STENCIL_READ:
+        img_refs_[ref_idx].history_.emplace(pass_idx,                                                //
+                                            std::pair(vk::ImageLayout::eDepthStencilReadOnlyOptimal, //
+                                                      vk::AccessFlagBits2::eDepthStencilAttachmentWrite));
+        break;
+    case DEPTH_READ | STENCIL_WRITE:
+        img_refs_[ref_idx].history_.emplace(pass_idx,                                                          //
+                                            std::pair(vk::ImageLayout::eDepthReadOnlyStencilAttachmentOptimal, //
+                                                      vk::AccessFlagBits2::eDepthStencilAttachmentWrite));
+        break;
+    case DEPTH_WRITE | STENCIL_READ:
+        img_refs_[ref_idx].history_.emplace(pass_idx,                                                          //
+                                            std::pair(vk::ImageLayout::eDepthAttachmentStencilReadOnlyOptimal, //
+                                                      vk::AccessFlagBits2::eDepthStencilAttachmentWrite));
+        break;
+    case DEPTH_WRITE | STENCIL_WRITE:
+        img_refs_[ref_idx].history_.emplace(pass_idx,                                                  //
+                                            std::pair(vk::ImageLayout::eDepthStencilAttachmentOptimal, //
+                                                      vk::AccessFlagBits2::eDepthStencilAttachmentWrite));
+        break;
+    default:
+        break;
+    }
+}
+
+void fi::FrameGraph::reset()
+{
+    if (!loaded)
+    {
+        loaded = true;
+    }
+
+    for (FrameImageRef& ref : img_refs_)
+    {
+        ref.history_.clear();
+    }
+}
