025c0ac464222f8a220b1ba88cc7c8ba40c308f1
Felix Xing <felixaszx@outlook.com>
2024-08-23 11:01:27 -0400

    remove resource code

--- include/engine/resource.hpp
+++ include/engine/resource.hpp
@@ -8,69 +8,7 @@
 
 namespace fi
 {
-    struct InstancingInfo
-    {
-        InstanceIdx first_matrix_ = EMPTY;
-        uint32_t instance_count_ = 0;
-    };
 
-    struct SceneRenderableRef
-    {
-        glm::mat4* matrix_ = nullptr;
-    };
-
-    struct SceneRenderable
-    {
-        uint32_t* bounding_radius_ = nullptr;
-        InstancingInfo* instancing_info_ = nullptr;
-        std::vector<SceneRenderableRef> refs_{};
-    };
-
-    class SceneResources : private GraphicsObject
-    {
-      private:
-        struct BufferOffsets
-        {
-            vk::DeviceSize instancing_infos_ = 0;
-            vk::DeviceSize instancing_matrices_ = 0;
-            vk::DeviceSize bounding_radius_ = 0;
-        };
-
-
-      public:
-        uint32_t ext_instance_count_ = 0;
-        std::unique_ptr<ResDetails> res_detail_;               // set 0 in graphics
-        std::unique_ptr<ResStructure> res_structure_{nullptr}; // set 1 in graphics
-        std::unique_ptr<ResSkinDetails> res_skin_{nullptr};    // set 2 in graphics
-        std::vector<std::vector<ResAnimation>> res_anims_{};
-        std::vector<SceneRenderable> renderables_{};
-
-        std::array<vk::DescriptorPoolSize, 1> des_sizes_{};
-        vk::DescriptorSetLayout set_layout_{};
-        vk::DescriptorSet des_set_{};
-
-        // storage
-        std::vector<InstancingInfo> instancing_infos_{}; // binding 1
-        std::vector<glm::mat4> instancing_matrices_{};   // binding 2
-        std::vector<uint32_t> bounding_radius_{};        // binding 3
-        std::unique_ptr<Buffer<BufferOffsets, storage, seq_write, host_coherent, presistant>> buffer_{nullptr};
-
-        std::filesystem::path res_path_ = "";
-
-        SceneResources(const std::filesystem::path& res_path, uint32_t max_instances = 0);
-        ~SceneResources();
-
-        void update_data();
-        void allocate_descriptor(vk::DescriptorPool des_pool);
-        void bind_res(vk::CommandBuffer cmd,
-                      vk::PipelineBindPoint bind_point, //
-                      vk::PipelineLayout pipeline_layout,
-                      uint32_t des_set);
-        void compute(vk::CommandBuffer cmd, const glm::uvec3& work_group = {0, 0, 0});
-        SceneRenderableRef* add_renderable_ref(PrimIdx renderable_idx);
-    };
-
-    using ECSRenderable = SceneRenderableRef;
 }; // namespace fi
 
 #endif // ENGINE_RESOURCE_HPP
