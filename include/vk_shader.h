c3dcf8dba288e079113055fffacbe1a2230d4d87
Felix Xing <101950667+felixaszx@users.noreply.github.com>
2024-09-20 20:29:11 -0400

    add ppeline meta strut

--- include/vk_shader.h
+++ include/vk_shader.h
@@ -21,4 +21,40 @@ typedef struct vk_shader
 DEFINE_OBJ(vk_shader, vk_ctx* ctx, const char* file_path, VkShaderStageFlags stage);
 DEFINE_OBJ_DELETE(vk_shader);
 
+enum vk_pl_stage
+{
+    GFX_PIPELINE_STAGE_VERT,
+    GFX_PIPELINE_STAGE_FRAG,
+    GFX_PIPELINE_STAGE_GEOM,
+    GFX_PIPELINE_STAGE_COMP = 0,
+};
+
+typedef struct vk_pl
+{
+    VkPipeline pl_;
+    VkPipelineLayout layout_;
+} vk_pl;
+
+typedef struct vk_gfx_pl
+{
+    vk_pl pl_;
+    VkPipelineShaderStageCreateInfo stages_[3];
+    VkPipelineRenderingCreateInfo atchms_;
+    VkPipelineVertexInputStateCreateInfo vtx_input_;
+    VkPipelineInputAssemblyStateCreateInfo input_asm_;
+    VkPipelineTessellationStateCreateInfo tessellation_;
+    VkPipelineViewportStateCreateInfo viewport_;
+    VkPipelineRasterizationStateCreateInfo rasterizer_;
+    VkPipelineMultisampleStateCreateInfo multi_sample_;
+    VkPipelineDepthStencilStateCreateInfo depth_stencil_;
+    VkPipelineColorBlendStateCreateInfo color_blend_;
+    VkPipelineDynamicStateCreateInfo dynamic_state_;
+} vk_gfx_pl;
+
+typedef struct vk_comp_pl
+{
+    vk_pl pl_;
+    VkPipelineShaderStageCreateInfo stages_[1];
+} vk_comp_pl;
+
 #endif // INCLUDE_VK_SHADER_H
